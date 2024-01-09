417141c87d7bfb16e08f3c9205187f42d5f53208
hoyun06 <hoyun06@gmail.com>
2024-01-09 22:25:39 +0900

    feat: Message 엔티티 클래스 생성 (#3)

---
+++ src/main/java/com/codot/link/domains/message/domain/Message.java
@@ -0,0 +1,38 @@
+package com.codot.link.domains.message.domain;
+
+import com.codot.link.domains.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Message {
+
+	@Id
+	@Column(name = "message_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "sender_id")
+	private User sender;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "receiver_id")
+	private User receiver;
+
+	private String title;
+
+	private String content;
+}
