978e64d69864758119163dbbb3a8aa21b6e9af5d
hoyun06 <hoyun06@gmail.com>
2024-01-09 22:35:51 +0900

    feat: UserRepository 인터페이스 생성 (#3)

---
+++ src/main/java/com/codot/link/domains/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.codot.link.domains.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codot.link.domains.user.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
