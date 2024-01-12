e9333a39529e19e7f3215e3e15ab30628de1dadb
hoyun06 <hoyun06@gmail.com>
2024-01-12 17:06:36 +0900

    feat: CustomExceptionResponse 클래스 생성 (#5)

---
+++ src/main/java/com/codot/link/common/exception/dto/response/CustomExceptionResponse.java
@@ -0,0 +1,32 @@
+package com.codot.link.common.exception.dto.response;
+
+import static java.time.LocalDateTime.*;
+
+import java.time.LocalDateTime;
+
+import com.codot.link.common.exception.model.CustomException;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CustomExceptionResponse {
+
+	private final LocalDateTime time;
+	private final String message;
+	private final String detail;
+
+	@Builder(access = AccessLevel.PRIVATE)
+	public CustomExceptionResponse(CustomException exception) {
+		time = now();
+		message = exception.getErrorCode().getMessage();
+		detail = exception.getDetail();
+	}
+
+	public static CustomExceptionResponse from(CustomException exception) {
+		return CustomExceptionResponse.builder()
+			.exception(exception)
+			.build();
+	}
+}
