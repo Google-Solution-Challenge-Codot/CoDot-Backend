0f19dc80960c07a8824e867c3bf6bf5025e58ba5
hoyun06 <hoyun06@gmail.com>
2024-01-12 17:30:21 +0900

    feat: GlobalExceptionHandler에 RuntimeException에 대한 처리 추가 (#5)

--- src/main/java/com/codot/link/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/codot/link/common/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.codot.link.common.exception.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,5 +16,12 @@ public class GlobalExceptionHandler {
 		return ResponseEntity
 			.status(exception.getErrorCode().getStatusCode())
 			.body(CustomExceptionResponse.from(exception));
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<String> customExceptionResponse(RuntimeException exception) {
+		return ResponseEntity
+			.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.body(exception.getMessage());
 	}
 }
