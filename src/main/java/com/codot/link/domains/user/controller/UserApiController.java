bcdf2acfa676fb4ca5af66a09a68533351b78dc5
hoyun06 <hoyun06@gmail.com>
2024-01-13 16:29:53 +0900

    feat: UserApiController 메서드 추가 (#9)
    - ResponseEntity<Void> userDelete(@Valid @RequestBody UserDeleteRequest request)

--- src/main/java/com/codot/link/domains/user/controller/UserApiController.java
+++ src/main/java/com/codot/link/domains/user/controller/UserApiController.java
@@ -3,6 +3,7 @@ package com.codot.link.domains.user.controller;
 import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codot.link.domains.user.dto.request.UserDeleteRequest;
 import com.codot.link.domains.user.dto.request.UserSignupRequest;
 import com.codot.link.domains.user.dto.request.UserUpdateRequest;
 import com.codot.link.domains.user.dto.response.UserInfoResponse;
@@ -50,6 +52,17 @@ public class UserApiController {
 		Long userId = 1L;
 
 		userService.userUpdate(userId, request);
+		return ResponseEntity
+			.noContent()
+			.build();
+	}
+
+	@DeleteMapping
+	public ResponseEntity<Void> userDelete(@Valid @RequestBody UserDeleteRequest request) {
+		// TODO: user_id 하드코딩 제거하고 header에서 값 추출하여 사용하기
+		Long userId = 1L;
+
+		userService.userDelete(userId, request);
 		return ResponseEntity
 			.noContent()
 			.build();
