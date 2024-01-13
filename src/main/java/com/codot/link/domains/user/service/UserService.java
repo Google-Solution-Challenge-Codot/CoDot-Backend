6af36dc8b61f927eff1233d3ddbb32df36f7ce2f
hoyun06 <hoyun06@gmail.com>
2024-01-13 14:01:43 +0900

    feat: UserService 메서드 추가 (#9)
    - userSignup(UserSignupRequest request)
    - validateNicknameDuplicate(String nickname)

--- src/main/java/com/codot/link/domains/user/service/UserService.java
+++ src/main/java/com/codot/link/domains/user/service/UserService.java
@@ -1,8 +1,13 @@
 package com.codot.link.domains.user.service;
 
+import static com.codot.link.common.exception.model.ErrorCode.*;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.codot.link.common.exception.model.CustomException;
+import com.codot.link.domains.user.domain.User;
+import com.codot.link.domains.user.dto.request.UserSignupRequest;
 import com.codot.link.domains.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,4 +18,15 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
 	private final UserRepository userRepository;
+
+	public void userSignup(UserSignupRequest request) {
+		validateNicknameDuplicate(request.getNickname());
+		userRepository.save(User.from(request));
+	}
+
+	private void validateNicknameDuplicate(String nickname) {
+		if (userRepository.existsByNickname(nickname)) {
+			throw CustomException.from(DUPLICATE_USER_NICKNAME);
+		}
+	}
 }
