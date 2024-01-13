ffc5d9104c2bf748e56e28ea74a9904296ae2b15
hoyun06 <hoyun06@gmail.com>
2024-01-13 15:20:15 +0900

    feat: UserService 메서드 추가 (#9)
    - UserInfoResponse userInfo(Long userId)
    - User findOne(Long userId)

--- src/main/java/com/codot/link/domains/user/service/UserService.java
+++ src/main/java/com/codot/link/domains/user/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.codot.link.common.exception.model.CustomException;
 import com.codot.link.domains.user.domain.User;
 import com.codot.link.domains.user.dto.request.UserSignupRequest;
+import com.codot.link.domains.user.dto.response.UserInfoResponse;
 import com.codot.link.domains.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -28,5 +29,15 @@ public class UserService {
 		if (userRepository.existsByNickname(nickname)) {
 			throw CustomException.from(DUPLICATE_USER_NICKNAME);
 		}
+	}
+
+	public UserInfoResponse userInfo(Long userId) {
+		User user = findOne(userId);
+		return UserInfoResponse.from(user);
+	}
+
+	private User findOne(Long userId) {
+		return userRepository.findById(userId)
+			.orElseThrow(() -> CustomException.from(USER_NOT_FOUND));
 	}
 }
