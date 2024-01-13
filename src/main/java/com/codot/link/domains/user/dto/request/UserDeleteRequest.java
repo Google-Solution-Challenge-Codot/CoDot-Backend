fe8e56dc5b163469109d1cba9f2afe837dbf82b9
hoyun06 <hoyun06@gmail.com>
2024-01-13 16:28:19 +0900

    feat: UserDeleteRequest 생성 (#9)

---
+++ src/main/java/com/codot/link/domains/user/dto/request/UserDeleteRequest.java
@@ -0,0 +1,29 @@
+package com.codot.link.domains.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserDeleteRequest {
+
+	@NotEmpty(message = "이메일은 필수이며 공백 문자열은 허용되지 않습니다.")
+	@Email(message = "이메일 형식이 아닙니다.")
+	private String email;
+
+	// === Test 용도 === //
+	@Builder(access = AccessLevel.PRIVATE)
+	private UserDeleteRequest(String email) {
+		this.email = email;
+	}
+
+	public static UserDeleteRequest from(String email) {
+		return UserDeleteRequest.builder()
+			.email(email)
+			.build();
+	}
+}
