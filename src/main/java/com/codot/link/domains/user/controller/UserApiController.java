47aa9f6a94bab0e6bf64714c57178068c9fe4237
hoyun06 <hoyun06@gmail.com>
2024-01-13 16:07:47 +0900

    feat: UserApiController 메서드 추가 (#9)
    - ResponseEntity<Void> userUpdate(@Valid @RequestBody UserUpdateRequest request)

--- src/main/java/com/codot/link/domains/user/controller/UserApiController.java
+++ src/main/java/com/codot/link/domains/user/controller/UserApiController.java
@@ -4,12 +4,14 @@ import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codot.link.domains.user.dto.request.UserSignupRequest;
+import com.codot.link.domains.user.dto.request.UserUpdateRequest;
 import com.codot.link.domains.user.dto.response.UserInfoResponse;
 import com.codot.link.domains.user.service.UserService;
 
@@ -35,10 +37,21 @@ public class UserApiController {
 	public ResponseEntity<UserInfoResponse> userInfo() {
 		// TODO: user_id 하드코딩 제거하고 header에서 값 추출하여 사용하기
 		Long userId = 1L;
-		
+
 		UserInfoResponse response = userService.userInfo(userId);
 		return ResponseEntity
 			.status(OK)
 			.body(response);
+	}
+
+	@PatchMapping
+	public ResponseEntity<Void> userUpdate(@Valid @RequestBody UserUpdateRequest request) {
+		// TODO: user_id 하드코딩 제거하고 header에서 값 추출하여 사용하기
+		Long userId = 1L;
+
+		userService.userUpdate(userId, request);
+		return ResponseEntity
+			.noContent()
+			.build();
 	}
 }
