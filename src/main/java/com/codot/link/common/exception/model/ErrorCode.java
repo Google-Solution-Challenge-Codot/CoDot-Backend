68e6f5eabbf14a853b96a32d417d328f4f44e4f0
hoyun06 <hoyun06@gmail.com>
2024-01-13 15:21:19 +0900

    feat: ErrorCode 상수 추가 (#9)
    - USER_NOT_FOUND

--- src/main/java/com/codot/link/common/exception/model/ErrorCode.java
+++ src/main/java/com/codot/link/common/exception/model/ErrorCode.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
+	// 404 NOT_FOUND
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+
 	// 409 CONFLICT
 	DUPLICATE_USER_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다.");
 
