fd1e69f41c389ed91df1f922517150529bc286e9
hoyun06 <hoyun06@gmail.com>
2024-01-14 20:39:30 +0900

    feat: LinkApiController 메서드 추가 (#15)
    - ResponseEntity<FriendRequestListResponse> friendRequestInfo(@RequestHeader("user-id") Long userId)

--- src/main/java/com/codot/link/domains/link/controller/LinkApiController.java
+++ src/main/java/com/codot/link/domains/link/controller/LinkApiController.java
@@ -3,6 +3,7 @@ package com.codot.link.domains.link.controller;
 import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codot.link.domains.link.dto.request.FriendRequest;
+import com.codot.link.domains.link.dto.response.FriendRequestListResponse;
 import com.codot.link.domains.link.service.LinkService;
 
 import jakarta.validation.Valid;
@@ -29,5 +31,13 @@ public class LinkApiController {
 		return ResponseEntity
 			.status(CREATED)
 			.build();
+	}
+
+	@GetMapping("/request")
+	public ResponseEntity<FriendRequestListResponse> friendRequestInfo(@RequestHeader("user-id") Long userId) {
+		FriendRequestListResponse response = linkService.friendRequestList(userId);
+		return ResponseEntity
+			.status(OK)
+			.body(response);
 	}
 }
