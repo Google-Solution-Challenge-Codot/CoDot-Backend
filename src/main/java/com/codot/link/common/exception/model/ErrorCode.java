b0762c77b880b127276f439a453f49a12e1f11e6
hoyun06 <hoyun06@gmail.com>
2024-01-14 16:48:18 +0900

    feat: ErrorCode 상수 추가 (#15)
    - EXISTING_FRIEND_REQUEST

--- src/main/java/com/codot/link/common/exception/model/ErrorCode.java
+++ src/main/java/com/codot/link/common/exception/model/ErrorCode.java
@@ -14,7 +14,8 @@ public enum ErrorCode {
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
 
 	// 409 CONFLICT
-	DUPLICATE_USER_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다.");
+	DUPLICATE_USER_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
+	EXISTING_FRIEND_REQUEST(HttpStatus.CONFLICT, "친구 추가 요청이 이미 존재합니다.");
 
 	private final String message;
 	private final int statusCode;
