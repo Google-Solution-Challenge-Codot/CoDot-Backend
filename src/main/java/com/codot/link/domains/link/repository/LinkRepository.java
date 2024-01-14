1a92c0773e5d14444f8883bf80eb3da8f5a081f7
hoyun06 <hoyun06@gmail.com>
2024-01-14 16:47:56 +0900

    feat: LinkRepository 메서드 추가 (#15)
    - boolean existsByFromAndTo(User from, User to)

--- src/main/java/com/codot/link/domains/link/repository/LinkRepository.java
+++ src/main/java/com/codot/link/domains/link/repository/LinkRepository.java
@@ -3,6 +3,9 @@ package com.codot.link.domains.link.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codot.link.domains.link.domain.Link;
+import com.codot.link.domains.user.domain.User;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
+
+	boolean existsByFromAndTo(User from, User to);
 }
