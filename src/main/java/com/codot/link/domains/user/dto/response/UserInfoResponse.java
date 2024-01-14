73fc9f23e53d048b1be2d4e8932d0ba05725a169
hoyun06 <hoyun06@gmail.com>
2024-01-14 14:23:14 +0900

    refactor: UserInfoResponse에 변경 사항 반영 (#11)

--- src/main/java/com/codot/link/domains/user/dto/response/UserInfoResponse.java
+++ src/main/java/com/codot/link/domains/user/dto/response/UserInfoResponse.java
@@ -1,6 +1,9 @@
 package com.codot.link.domains.user.dto.response;
 
+import com.codot.link.domains.user.domain.GraduationStatus;
 import com.codot.link.domains.user.domain.User;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,26 +12,34 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserInfoResponse {
 
+	private String filePath;
 	private String nickname;
 	private String university;
 	private String department;
+	private GraduationStatus graduationStatus;
 	private String introduction;
 
 	@Builder(access = AccessLevel.PRIVATE)
-	private UserInfoResponse(String nickname, String university, String department, String introduction) {
+	private UserInfoResponse(String filePath, String nickname, String university, String department,
+		GraduationStatus graduationStatus, String introduction) {
+		this.filePath = filePath;
 		this.nickname = nickname;
 		this.university = university;
 		this.department = department;
+		this.graduationStatus = graduationStatus;
 		this.introduction = introduction;
 	}
 
 	public static UserInfoResponse from(User user) {
 		return UserInfoResponse.builder()
+			.filePath(user.getFilePath())
 			.nickname(user.getNickname())
 			.university(user.getUniversity())
 			.department(user.getDepartment())
+			.graduationStatus(user.getGraduationStatus())
 			.introduction(user.getIntroduction())
 			.build();
 	}
