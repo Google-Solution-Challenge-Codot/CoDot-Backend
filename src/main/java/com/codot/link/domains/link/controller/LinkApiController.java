b462a2a52a4216c3106346b375a929736e9715d4
hoyun06 <hoyun06@gmail.com>
2024-01-14 16:46:33 +0900

    feat: LinkApiController 생성 (#15)

---
+++ src/main/java/com/codot/link/domains/link/controller/LinkApiController.java
@@ -0,0 +1,33 @@
+package com.codot.link.domains.link.controller;
+
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codot.link.domains.link.dto.request.FriendRequest;
+import com.codot.link.domains.link.service.LinkService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/links")
+public class LinkApiController {
+
+	private final LinkService linkService;
+
+	@PostMapping("/request")
+	public ResponseEntity<Void> friendRequest(@Valid @RequestBody FriendRequest request,
+		@RequestHeader("user-id") Long userId) {
+		linkService.createFriendRequest(userId, request);
+		return ResponseEntity
+			.status(CREATED)
+			.build();
+	}
+}
