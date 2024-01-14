52774d4b8a082517eddce849d04471817fbe34da
hoyun06 <hoyun06@gmail.com>
2024-01-14 20:38:23 +0900

    feat: LinkRepository 메서드 추가 (#15)
    - List<Link> findAllByTo_IdAndStatus(Long toId, Status status)

--- src/main/java/com/codot/link/domains/link/repository/LinkRepository.java
+++ src/main/java/com/codot/link/domains/link/repository/LinkRepository.java
@@ -1,11 +1,16 @@
 package com.codot.link.domains.link.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codot.link.domains.link.domain.Link;
+import com.codot.link.domains.link.domain.Status;
 import com.codot.link.domains.user.domain.User;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
 
 	boolean existsByFromAndTo(User from, User to);
+
+	List<Link> findAllByTo_IdAndStatus(Long toId, Status status);
 }
