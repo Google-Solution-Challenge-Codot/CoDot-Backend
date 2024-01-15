1d9ca8bf548a08a43ee43726cb222054e8b39dbb
hoyun06 <hoyun06@gmail.com>
2024-01-15 10:57:19 +0900

    feat: MessageRepository 메서드 추가 (#17)
    - List<Message> findAllByReceiver_Id(Long receiverId)

--- src/main/java/com/codot/link/domains/message/repository/MessageRepository.java
+++ src/main/java/com/codot/link/domains/message/repository/MessageRepository.java
@@ -1,8 +1,11 @@
 package com.codot.link.domains.message.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codot.link.domains.message.domain.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+	List<Message> findAllByReceiver_Id(Long receiverId);
 }
