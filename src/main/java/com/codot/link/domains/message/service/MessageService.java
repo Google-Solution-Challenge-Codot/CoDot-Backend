e41a35c9a5373bfa51297c16c9997c0c8f132c2d
hoyun06 <hoyun06@gmail.com>
2024-01-15 10:41:50 +0900

    feat: MessageService 메서드 추가 (#17)
    - void sendMessage(Long userId, MessageCreateRequest request)
    - User findUserByUserId(Long userId)

--- src/main/java/com/codot/link/domains/message/service/MessageService.java
+++ src/main/java/com/codot/link/domains/message/service/MessageService.java
@@ -1,9 +1,16 @@
 package com.codot.link.domains.message.service;
 
+import static com.codot.link.common.exception.model.ErrorCode.*;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.codot.link.common.exception.model.CustomException;
+import com.codot.link.domains.message.domain.Message;
+import com.codot.link.domains.message.dto.request.MessageCreateRequest;
 import com.codot.link.domains.message.repository.MessageRepository;
+import com.codot.link.domains.user.domain.User;
+import com.codot.link.domains.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,4 +20,17 @@ import lombok.RequiredArgsConstructor;
 public class MessageService {
 
 	private final MessageRepository messageRepository;
+	private final UserRepository userRepository;
+
+	public void sendMessage(Long userId, MessageCreateRequest request) {
+		User sender = findUserByUserId(userId);
+		User receiver = findUserByUserId(request.getTargetId());
+
+		messageRepository.save(Message.of(sender, receiver, request));
+	}
+
+	private User findUserByUserId(Long userId) {
+		return userRepository.findById(userId)
+			.orElseThrow(() -> CustomException.from(USER_NOT_FOUND));
+	}
 }
