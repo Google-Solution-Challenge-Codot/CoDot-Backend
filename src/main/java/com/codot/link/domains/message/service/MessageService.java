d08416217026c4c7a826d6e2d652677b83ccb26d
hoyun06 <hoyun06@gmail.com>
2024-01-15 10:57:42 +0900

    feat: MessageService 메서드 추가 (#17)
    - MessageListResponse receivedMessageList(Long userId)

--- src/main/java/com/codot/link/domains/message/service/MessageService.java
+++ src/main/java/com/codot/link/domains/message/service/MessageService.java
@@ -2,12 +2,16 @@ package com.codot.link.domains.message.service;
 
 import static com.codot.link.common.exception.model.ErrorCode.*;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.codot.link.common.exception.model.CustomException;
 import com.codot.link.domains.message.domain.Message;
 import com.codot.link.domains.message.dto.request.MessageCreateRequest;
+import com.codot.link.domains.message.dto.response.MessageListResponse;
+import com.codot.link.domains.message.dto.response.MessageResponse;
 import com.codot.link.domains.message.repository.MessageRepository;
 import com.codot.link.domains.user.domain.User;
 import com.codot.link.domains.user.repository.UserRepository;
@@ -32,5 +36,14 @@ public class MessageService {
 	private User findUserByUserId(Long userId) {
 		return userRepository.findById(userId)
 			.orElseThrow(() -> CustomException.from(USER_NOT_FOUND));
+	}
+
+	public MessageListResponse receivedMessageList(Long userId) {
+		List<Message> byReceiverId = messageRepository.findAllByReceiver_Id(userId);
+
+		List<MessageResponse> messages = byReceiverId.stream()
+			.map(MessageResponse::from)
+			.toList();
+		return MessageListResponse.from(messages);
 	}
 }
