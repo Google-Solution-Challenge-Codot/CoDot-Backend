80f5f3fc0500beff3b13b7740ffda4d946a83dea
hoyun06 <hoyun06@gmail.com>
2024-01-15 11:17:16 +0900

    feat: MessageApiController 메서드 추가 (#17)
    - ResponseEntity<Void> deleteMessage(@PathVariable("message_id") Long messageId)

--- src/main/java/com/codot/link/domains/message/controller/MessageApiController.java
+++ src/main/java/com/codot/link/domains/message/controller/MessageApiController.java
@@ -3,7 +3,9 @@ package com.codot.link.domains.message.controller;
 import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -39,5 +41,13 @@ public class MessageApiController {
 		return ResponseEntity
 			.status(OK)
 			.body(response);
+	}
+
+	@DeleteMapping("/{message_id}")
+	public ResponseEntity<Void> deleteMessage(@PathVariable("message_id") Long messageId) {
+		messageService.deleteMessage(messageId);
+		return ResponseEntity
+			.noContent()
+			.build();
 	}
 }
