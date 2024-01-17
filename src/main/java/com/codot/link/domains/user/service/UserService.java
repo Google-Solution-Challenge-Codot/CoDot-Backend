fef1f9db7ac851a7d446f977eb2786f88e633e24
hoyun06 <hoyun06@gmail.com>
2024-01-17 16:00:22 +0900

    feat: UserService 메서드 추가 (#19)
    - TwoHopListResponse userHome(Long userId)

--- src/main/java/com/codot/link/domains/user/service/UserService.java
+++ src/main/java/com/codot/link/domains/user/service/UserService.java
@@ -2,14 +2,20 @@ package com.codot.link.domains.user.service;
 
 import static com.codot.link.common.exception.model.ErrorCode.*;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.codot.link.common.exception.model.CustomException;
+import com.codot.link.domains.link.domain.Link;
+import com.codot.link.domains.link.repository.LinkRepository;
 import com.codot.link.domains.user.domain.User;
 import com.codot.link.domains.user.dto.request.UserDeleteRequest;
 import com.codot.link.domains.user.dto.request.UserSignupRequest;
 import com.codot.link.domains.user.dto.request.UserUpdateRequest;
+import com.codot.link.domains.user.dto.response.TwoHopListResponse;
+import com.codot.link.domains.user.dto.response.TwoHopResponse;
 import com.codot.link.domains.user.dto.response.UserInfoResponse;
 import com.codot.link.domains.user.repository.UserRepository;
 
@@ -21,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
 	private final UserRepository userRepository;
+	private final LinkRepository linkRepository;
 
 	public void userSignup(UserSignupRequest request) {
 		validateNicknameDuplicate(request.getNickname());
@@ -62,5 +69,14 @@ public class UserService {
 		if (userRepository.existsByNickname(nickname)) {
 			throw CustomException.from(DUPLICATE_USER_NICKNAME);
 		}
+	}
+
+	public TwoHopListResponse userHome(Long userId) {
+		List<Link> links = linkRepository.findTwoHops(userId);
+
+		List<TwoHopResponse> twoHops = links.stream()
+			.map(TwoHopResponse::from)
+			.toList();
+		return TwoHopListResponse.from(twoHops);
 	}
 }
