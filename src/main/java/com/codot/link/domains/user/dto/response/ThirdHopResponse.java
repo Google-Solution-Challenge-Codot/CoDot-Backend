d842382d494f34d8ecfc6664ab8de321df4cbaec
hoyun06 <hoyun06@gmail.com>
2024-01-18 18:38:10 +0900

    feat: 특정 사용자로부터 3 hop 떨어진 사용자를 나타내는 ThirdHopResponse dto 생성 (#21)

---
+++ src/main/java/com/codot/link/domains/user/dto/response/ThirdHopResponse.java
@@ -0,0 +1,35 @@
+package com.codot.link.domains.user.dto.response;
+
+import com.codot.link.domains.link.domain.ConnectionPoint;
+import com.codot.link.domains.user.domain.User;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ThirdHopResponse extends HopResponse {
+
+	private ConnectionPoint secondAndThirdHopConnectionPoint;
+
+	@Builder(access = AccessLevel.PRIVATE)
+	public ThirdHopResponse(Long userId, String filePath, String nickname,
+		ConnectionPoint connectionPoint) {
+		super(userId, filePath, nickname);
+		this.secondAndThirdHopConnectionPoint = connectionPoint;
+	}
+
+	public static ThirdHopResponse of(User user, ConnectionPoint connectionPoint) {
+		return ThirdHopResponse.builder()
+			.userId(user.getId())
+			.filePath(user.getFilePath())
+			.nickname(user.getNickname())
+			.connectionPoint(connectionPoint)
+			.build();
+	}
+}
