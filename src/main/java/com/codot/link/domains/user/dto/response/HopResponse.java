16042f40e7e3944e399e39fde1125b832b8b4583
hoyun06 <hoyun06@gmail.com>
2024-01-18 18:37:08 +0900

    feat: 각 hop을 나타내는 사용자 정보를 담는 HopResponse dto 생성 (#21)

---
+++ src/main/java/com/codot/link/domains/user/dto/response/HopResponse.java
@@ -0,0 +1,24 @@
+package com.codot.link.domains.user.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class HopResponse {
+
+	private Long userId;
+	private String filePath;
+	private String nickname;
+
+	protected HopResponse(Long userId, String filePath, String nickname) {
+		this.userId = userId;
+		this.filePath = filePath;
+		this.nickname = nickname;
+	}
+}
