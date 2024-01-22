76e2f2146d2e7cf246f1126ee15215439c34f228
hoyun06 <hoyun06@gmail.com>
2024-01-22 19:46:24 +0900

    feat: RefreshToken 생성 (#33)

---
+++ src/main/java/com/codot/link/domains/auth/domain/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.codot.link.domains.auth.domain;
+
+import com.codot.link.domains.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "refresh_token_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Column(nullable = false)
+	private String token;
+}
