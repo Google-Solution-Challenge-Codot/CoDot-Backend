11a438fc7768717d9a2cdf8ed0790621f2b13de7
hoyun06 <hoyun06@gmail.com>
2024-01-23 15:36:10 +0900

    feat: ErrorCode 상수 추가 (#38)
    - REFRESHTOKEN_NOT_MATCH

--- src/main/java/com/codot/link/common/exception/model/ErrorCode.java
+++ src/main/java/com/codot/link/common/exception/model/ErrorCode.java
@@ -1,5 +1,7 @@
 package com.codot.link.common.exception.model;
 
+import static org.springframework.http.HttpStatus.*;
+
 import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
@@ -8,21 +10,22 @@ import lombok.Getter;
 public enum ErrorCode {
 
 	//400 BAD_REQUEST
-	EMAIL_NOT_MATCH(HttpStatus.BAD_REQUEST, "이메일이 일치하지 않습니다."),
-	ID_NOT_PRESENT(HttpStatus.BAD_REQUEST, "헤더에 어떠한 id도 존재하지 않습니다."),
+	EMAIL_NOT_MATCH(BAD_REQUEST, "이메일이 일치하지 않습니다."),
+	ID_NOT_PRESENT(BAD_REQUEST, "헤더에 어떠한 id도 존재하지 않습니다."),
 
 	//401 UNAUTHORIZED
-	TOKEN_NOT_VALID(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다."),
+	TOKEN_NOT_VALID(UNAUTHORIZED, "토큰이 유효하지 않습니다."),
 
 	// 404 NOT_FOUND
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
-	LINK_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 LINK입니다."),
-	LOGIN_RECORD_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 Login Record입니다."),
+	USER_NOT_FOUND(NOT_FOUND, "존재하지 않는 사용자입니다."),
+	LINK_NOT_FOUND(NOT_FOUND, "존재하지 않는 LINK입니다."),
+	LOGIN_RECORD_NOT_FOUND(NOT_FOUND, "존재하지 않는 Login Record입니다."),
 
 	// 409 CONFLICT
-	DUPLICATE_USER_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
-	ALREADY_ACCEPTED_FRIEND_REQUEST(HttpStatus.CONFLICT, "이미 수락된 친구 추가 요청입니다."),
-	EXISTING_FRIEND_REQUEST(HttpStatus.CONFLICT, "친구 추가 요청이 이미 존재합니다.");
+	DUPLICATE_USER_NICKNAME(CONFLICT, "이미 존재하는 닉네임입니다."),
+	ALREADY_ACCEPTED_FRIEND_REQUEST(CONFLICT, "이미 수락된 친구 추가 요청입니다."),
+	EXISTING_FRIEND_REQUEST(CONFLICT, "친구 추가 요청이 이미 존재합니다."),
+	REFRESHTOKEN_NOT_MATCH(CONFLICT, "Refresh Token이 일치하지 않습니다.");
 
 	private final String message;
 	private final int statusCode;
