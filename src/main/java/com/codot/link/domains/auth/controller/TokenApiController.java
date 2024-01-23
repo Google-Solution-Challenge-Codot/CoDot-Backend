29835679a61a5d861b4b06063abff12338dbd97a
hoyun06 <97820785+hoyun06@users.noreply.github.com>
2024-01-23 16:51:58 +0900

    feat: Access Token 재발급 로직 구현 (#38) (#40)
    * feat: RefreshTokenRepository 메서드 추가 (#38)
    
    - boolean existsByUserAndToken(User user, String token)
    
    * feat: ErrorCode 상수 추가 (#38)
    
    - REFRESHTOKEN_NOT_MATCH
    
    * feat: TokenService 토큰 재발급 메서드 추가 (#38)
    
    * feat: TokenApiController "/reissue" 경로 요청 처리하는 메서드 추가 (#38)

--- src/main/java/com/codot/link/domains/auth/controller/TokenApiController.java
+++ src/main/java/com/codot/link/domains/auth/controller/TokenApiController.java
@@ -3,30 +3,41 @@ package com.codot.link.domains.auth.controller;
 import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codot.link.domains.auth.dto.request.IssueTokenRequest;
 import com.codot.link.domains.auth.dto.response.IssueTokenResponse;
 import com.codot.link.domains.auth.service.TokenService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/login")
 public class TokenApiController {
 
 	private final TokenService tokenService;
 
-	@PostMapping
+	@PostMapping("/login")
 	public ResponseEntity<IssueTokenResponse> issueJwt(@Valid @RequestBody IssueTokenRequest request) {
 		IssueTokenResponse response = tokenService.issueJwt(request);
 		return ResponseEntity
 			.status(CREATED)
 			.body(response);
+	}
+
+	@GetMapping("/reissue")
+	public ResponseEntity<Void> reissueJwt(@RequestHeader("user-id") Long userId,
+		@RequestHeader("Authorization") String refreshToken, HttpServletResponse response) {
+		String token = tokenService.reissueJwt(userId, refreshToken);
+		response.setHeader("access-token", token);
+		return ResponseEntity
+			.status(OK)
+			.build();
 	}
 }
--- src/main/java/com/codot/link/domains/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/codot/link/domains/auth/repository/RefreshTokenRepository.java
@@ -3,6 +3,8 @@ package com.codot.link.domains.auth.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codot.link.domains.auth.domain.RefreshToken;
+import com.codot.link.domains.user.domain.User;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+	boolean existsByUserAndToken(User user, String token);
 }
