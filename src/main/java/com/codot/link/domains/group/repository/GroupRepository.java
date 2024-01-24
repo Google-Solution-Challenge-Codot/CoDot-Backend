a9eba7f781c0dfaac4cc0bb32b0ac5e13ce2eeab
hoyun06 <hoyun06@gmail.com>
2024-01-24 19:13:04 +0900

    feat: GroupRepository GROUP2_MODIFY1 관련 조회 메서드(네이티브 쿼리) 추가 (#43)
    - Optional<Group> findByUserIdAndGroupId(@Param("userId") Long userId, @Param("groupId") Long groupId)

--- src/main/java/com/codot/link/domains/group/repository/GroupRepository.java
+++ src/main/java/com/codot/link/domains/group/repository/GroupRepository.java
@@ -1,10 +1,18 @@
 package com.codot.link.domains.group.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.codot.link.domains.group.domain.Group;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
 	boolean existsByName(String name);
+
+	@Query(value = "select g.* from groups g "
+		+ "where exists (select 1 from group_user gu where gu.user_id = :userId and gu.group_id = :groupId and gu.role = 'HOST')", nativeQuery = true)
+	Optional<Group> findByUserIdAndGroupId(@Param("userId") Long userId, @Param("groupId") Long groupId);
 }
