2976eabf59809ed97abc91180037565766744646
hoyun06 <hoyun06@gmail.com>
2024-01-24 23:18:28 +0900

    feat: GroupUserRepository GROUP3_DELETE1 관련 메서드 추가 (#43)
    - List<GroupUser> findAllByGroup

--- src/main/java/com/codot/link/domains/group/repository/GroupUserRepository.java
+++ src/main/java/com/codot/link/domains/group/repository/GroupUserRepository.java
@@ -1,8 +1,13 @@
 package com.codot.link.domains.group.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.codot.link.domains.group.domain.Group;
 import com.codot.link.domains.group.domain.GroupUser;
 
 public interface GroupUserRepository extends JpaRepository<GroupUser, Long> {
+
+	List<GroupUser> findAllByGroup(Group group);
 }
