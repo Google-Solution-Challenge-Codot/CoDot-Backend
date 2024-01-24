cc0b708a1d3e2e54feff1b2c266c3c23762b7c32
hoyun06 <hoyun06@gmail.com>
2024-01-24 16:53:32 +0900

    feat: GroupService 메서드 추가 (#43)
    - void createGroup(Long userId, GroupCreateRequest request)
    - void validateGroupNameDuplicate(GroupCreateRequest request)
    - User findUserByUserId(Long userId)

--- src/main/java/com/codot/link/domains/group/service/GroupService.java
+++ src/main/java/com/codot/link/domains/group/service/GroupService.java
@@ -1,9 +1,19 @@
 package com.codot.link.domains.group.service;
 
+import static com.codot.link.common.exception.model.ErrorCode.*;
+import static com.codot.link.domains.group.domain.Role.*;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.codot.link.common.exception.model.CustomException;
+import com.codot.link.domains.group.domain.Group;
+import com.codot.link.domains.group.domain.GroupUser;
+import com.codot.link.domains.group.dto.request.GroupCreateRequest;
 import com.codot.link.domains.group.repository.GroupRepository;
+import com.codot.link.domains.group.repository.GroupUserRepository;
+import com.codot.link.domains.user.domain.User;
+import com.codot.link.domains.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +22,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class GroupService {
 
+	private final UserRepository userRepository;
 	private final GroupRepository groupRepository;
+	private final GroupUserRepository groupUserRepository;
+
+	public void createGroup(Long userId, GroupCreateRequest request) {
+		validateGroupNameDuplicate(request);
+
+		Group group = groupRepository.save(Group.from(request));
+		User user = findUserByUserId(userId);
+		
+		groupUserRepository.save(GroupUser.of(user, group, HOST, true));
+	}
+
+	private void validateGroupNameDuplicate(GroupCreateRequest request) {
+		if (groupRepository.existsByName(request.getName())) {
+			throw CustomException.from(DUPLICATE_GROUP_NAME);
+		}
+	}
+
+	private User findUserByUserId(Long userId) {
+		return userRepository.findById(userId)
+			.orElseThrow(() -> CustomException.from(USER_NOT_FOUND));
+	}
 }
