efd778986dec3d8e7a6dddfadb62ae6c56e59074
hoyun06 <hoyun06@gmail.com>
2024-01-24 16:51:37 +0900

    feat: GroupRepository 메서드 추가 (#43)
    - boolean existsByName(String name)

--- src/main/java/com/codot/link/domains/group/repository/GroupRepository.java
+++ src/main/java/com/codot/link/domains/group/repository/GroupRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.codot.link.domains.group.domain.Group;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
+
+	boolean existsByName(String name);
 }
