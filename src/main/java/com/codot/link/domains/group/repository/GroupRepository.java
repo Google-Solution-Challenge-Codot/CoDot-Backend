34d5877a0475494ef3a3a58aa0611f97888ba4d7
hoyun06 <hoyun06@gmail.com>
2024-01-25 15:27:38 +0900

    feat: GroupRepository GROUP2_MY_GROUP1 관련 메서드 추가 (#43)
    - List<MyGroupResult> findAllByUserAndAccepted(@Param("userId") Long userId, @Param("accepted") boolean accepted)

--- src/main/java/com/codot/link/domains/group/repository/GroupRepository.java
+++ src/main/java/com/codot/link/domains/group/repository/GroupRepository.java
@@ -1,11 +1,13 @@
 package com.codot.link.domains.group.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.codot.link.domains.group.dao.MyGroupResult;
 import com.codot.link.domains.group.domain.Group;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
@@ -30,4 +32,9 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
 		+ ") as u3 "
 		+ "where exists (select 1 from group_user gu where gu.user_id = u3.user_id and gu.group_id = :groupId and gu.accepted = true)", nativeQuery = true)
 	boolean isGroupJoinable(@Param("userId") Long userId, @Param("groupId") Long groupId);
+
+	@Query(value = "select g.group_id as groupId, g.name as groupName, g.description, gu.role from groups g "
+		+ "join group_user gu on g.group_id = gu.group_id "
+		+ "where gu.user_id = :userId and gu.accepted = :accepted", nativeQuery = true)
+	List<MyGroupResult> findAllByUserAndAccepted(@Param("userId") Long userId, @Param("accepted") boolean accepted);
 }
