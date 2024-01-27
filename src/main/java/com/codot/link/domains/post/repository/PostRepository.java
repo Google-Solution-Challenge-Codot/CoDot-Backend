a33f1a4351cf676d3c281a24fe4f13664460552a
hoyun06 <hoyun06@gmail.com>
2024-01-27 22:13:22 +0900

    feat: PostRepository POST2_INFO1 관련 조회 메서드(네이티브 쿼리) 추가 (#51)

--- src/main/java/com/codot/link/domains/post/repository/PostRepository.java
+++ src/main/java/com/codot/link/domains/post/repository/PostRepository.java
@@ -1,8 +1,16 @@
 package com.codot.link.domains.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.codot.link.domains.post.domain.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+	@Query(value = "select count(*) > 0 from posts p "
+		+ "join groups g on p.group_id = g.group_id "
+		+ "where p.post_id = :postId "
+		+ "and exists (select 1 from group_user gu where gu.group_id = g.group_id and gu.user_id = :userId and gu.accepted = true)", nativeQuery = true)
+	boolean canUserAccessPost(@Param("userId") Long userId, @Param("postId") Long postId);
 }
