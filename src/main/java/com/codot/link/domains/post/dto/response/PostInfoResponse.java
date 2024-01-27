71f4d9fa5638f5b3746390fa6e30577badc93cc0
hoyun06 <hoyun06@gmail.com>
2024-01-27 22:12:52 +0900

    feat: PostInfoResponse dto 생성 (#51)

---
+++ src/main/java/com/codot/link/domains/post/dto/response/PostInfoResponse.java
@@ -0,0 +1,48 @@
+package com.codot.link.domains.post.dto.response;
+
+import com.codot.link.domains.post.domain.Post;
+import com.codot.link.domains.user.domain.User;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PostInfoResponse {
+
+	private Long postId;
+	private String title;
+	private String content;
+	private Long postWriterId;
+	private String postWriterFilePath;
+	private String postWriterNickname;
+	// TODO: Comment 도메인 개발 완료 후 해당 게시물 Comment 정보도 포함하기
+
+	@Builder(access = AccessLevel.PRIVATE)
+	private PostInfoResponse(Long postId, String title, String content, Long postWriterId, String postWriterFilePath,
+		String postWriterNickname) {
+		this.postId = postId;
+		this.title = title;
+		this.content = content;
+		this.postWriterId = postWriterId;
+		this.postWriterFilePath = postWriterFilePath;
+		this.postWriterNickname = postWriterNickname;
+	}
+
+	public static PostInfoResponse from(Post post) {
+		User user = post.getUser();
+		return PostInfoResponse.builder()
+			.postId(post.getId())
+			.title(post.getTitle())
+			.content(post.getContent())
+			.postWriterId(user.getId())
+			.postWriterFilePath(user.getFilePath())
+			.postWriterNickname(user.getNickname())
+			.build();
+	}
+}
