c01499d16d9fe659242c4d311cd305865f241ba7
hoyun06 <hoyun06@gmail.com>
2024-01-27 22:59:18 +0900

    feat: PostRepository POST2_MODIFY1 관련 조회 메서드 추가 (#51)

--- src/main/java/com/codot/link/domains/post/repository/PostRepository.java
+++ src/main/java/com/codot/link/domains/post/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.codot.link.domains.post.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +18,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 	boolean canUserAccessPost(@Param("userId") Long userId, @Param("postId") Long postId);
 
 	List<Post> findAllByGroup_Id(Long groupId);
+
+	Optional<Post> findByIdAndUser_Id(Long postId, Long userId);
 }
