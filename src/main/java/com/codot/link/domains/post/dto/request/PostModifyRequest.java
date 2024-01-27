25550d008953a9362efe28f071198dfda6c36553
hoyun06 <hoyun06@gmail.com>
2024-01-27 23:01:12 +0900

    refactor: PostModifyRequest 불필요한 어노테이션 제거 (#51)

--- src/main/java/com/codot/link/domains/post/dto/request/PostModifyRequest.java
+++ src/main/java/com/codot/link/domains/post/dto/request/PostModifyRequest.java
@@ -1,8 +1,5 @@
 package com.codot.link.domains.post.dto.request;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +7,6 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PostModifyRequest {
 
 	private String title;
