85227d0355d2fbd4f17906adf62fe46be77cdd3b
hoyun06 <hoyun06@gmail.com>
2024-01-27 22:43:14 +0900

    feat: PostRepository POST2_LIST1 관련 조회 메서드 추가 (#51)

--- src/main/java/com/codot/link/domains/post/repository/PostRepository.java
+++ src/main/java/com/codot/link/domains/post/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.codot.link.domains.post.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +15,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 		+ "where p.post_id = :postId "
 		+ "and exists (select 1 from group_user gu where gu.group_id = g.group_id and gu.user_id = :userId and gu.accepted = true)", nativeQuery = true)
 	boolean canUserAccessPost(@Param("userId") Long userId, @Param("postId") Long postId);
+
+	List<Post> findAllByGroup_Id(Long groupId);
 }
