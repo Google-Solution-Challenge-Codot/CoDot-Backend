87a4e74e999503345ba87265226d5f036efae7dc
hoyun06 <hoyun06@gmail.com>
2024-01-28 16:36:26 +0900

    feat: CommentService COMMENT1_WRITE1 관련 메서드 추가 (#55)
    - void createComment(Long userId, CommentCreateRequest request)
    - void checkCommentWritingPermission(Long userId, CommentCreateRequest request)
    - User findUserByUserId(Long userId)
    - Post findPostByPostId(Long postId)

--- src/main/java/com/codot/link/domains/comment/service/CommentService.java
+++ src/main/java/com/codot/link/domains/comment/service/CommentService.java
@@ -1,9 +1,18 @@
 package com.codot.link.domains.comment.service;
 
+import static com.codot.link.common.exception.model.ErrorCode.*;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.codot.link.common.exception.model.CustomException;
+import com.codot.link.domains.comment.domain.Comment;
+import com.codot.link.domains.comment.dto.request.CommentCreateRequest;
 import com.codot.link.domains.comment.repository.CommentRepository;
+import com.codot.link.domains.post.domain.Post;
+import com.codot.link.domains.post.repository.PostRepository;
+import com.codot.link.domains.user.domain.User;
+import com.codot.link.domains.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +21,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CommentService {
 
+	private final UserRepository userRepository;
+	private final PostRepository postRepository;
 	private final CommentRepository commentRepository;
+
+	public void createComment(Long userId, CommentCreateRequest request) {
+		checkCommentWritingPermission(userId, request);
+
+		User user = findUserByUserId(userId);
+		Post post = findPostByPostId(request.getPostId());
+		commentRepository.save(Comment.of(user, post, request));
+	}
+
+	private void checkCommentWritingPermission(Long userId, CommentCreateRequest request) {
+		if (!postRepository.canUserAccessPost(userId, request.getPostId())) {
+			throw CustomException.of(NOT_GROUP_MEMBER, "자신이 속한 그룹의 게시물에만 댓글을 달 수 있습니다.");
+		}
+	}
+
+	private User findUserByUserId(Long userId) {
+		return userRepository.findById(userId)
+			.orElseThrow(() -> CustomException.from(USER_NOT_FOUND));
+	}
+
+	private Post findPostByPostId(Long postId) {
+		return postRepository.findById(postId)
+			.orElseThrow(() -> CustomException.from(POST_NOT_FOUND));
+	}
 }
