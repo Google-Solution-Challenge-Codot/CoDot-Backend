ad7e09512ee52b55cf2e5d89b2e823dc2e5a37b1
hoyun06 <hoyun06@gmail.com>
2024-01-28 23:35:56 +0900

    feat: Post 도메인과 Comment 도메인과의 양방향 연관관계 설정 및 cascade remove 적용 (#57)

--- src/main/java/com/codot/link/domains/post/domain/Post.java
+++ src/main/java/com/codot/link/domains/post/domain/Post.java
@@ -1,10 +1,15 @@
 package com.codot.link.domains.post.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.codot.link.domains.comment.domain.Comment;
 import com.codot.link.domains.group.domain.Group;
 import com.codot.link.domains.post.dto.request.PostCreateRequest;
 import com.codot.link.domains.post.dto.request.PostModifyRequest;
 import com.codot.link.domains.user.domain.User;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,6 +18,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -43,6 +49,9 @@ public class Post {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "group_id", nullable = false)
 	private Group group;
+
+	@OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+	private List<Comment> comments = new ArrayList<>();
 
 	@Builder(access = AccessLevel.PRIVATE)
 	private Post(String title, String content, User user, Group group) {
