a84c5748a6cdc98158903eafc8ca88e6d01d60ae
hoyun06 <hoyun06@gmail.com>
2024-01-28 19:37:11 +0900

    feat: SubCommentApiController SUB_COMMENT2_MODIFY1 관련 메서드 추가 (#56)
    - ResponseEntity<Void> modifySubComment(@RequestHeader("user-id") Long userId, @PathVariable("sub_comment_id") Long subCommentId, @Valid @RequestBody SubCommentModifyRequest request)

--- src/main/java/com/codot/link/domains/comment/controller/SubCommentApiController.java
+++ src/main/java/com/codot/link/domains/comment/controller/SubCommentApiController.java
@@ -3,6 +3,8 @@ package com.codot.link.domains.comment.controller;
 import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codot.link.domains.comment.dto.request.SubCommentCreateRequest;
+import com.codot.link.domains.comment.dto.request.SubCommentModifyRequest;
 import com.codot.link.domains.comment.service.SubCommentService;
 
 import jakarta.validation.Valid;
@@ -28,6 +31,15 @@ public class SubCommentApiController {
 		subCommentService.createSubComment(userId, request);
 		return ResponseEntity
 			.status(CREATED)
+			.build();
+	}
+
+	@PatchMapping("/{sub_comment_id}")
+	public ResponseEntity<Void> modifySubComment(@RequestHeader("user-id") Long userId,
+		@PathVariable("sub_comment_id") Long subCommentId, @Valid @RequestBody SubCommentModifyRequest request) {
+		subCommentService.modifySubComment(userId, subCommentId, request);
+		return ResponseEntity
+			.noContent()
 			.build();
 	}
 }
