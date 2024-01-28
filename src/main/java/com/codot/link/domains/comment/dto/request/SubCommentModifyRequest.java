0bd8277d6d3066f8a8e129997ebaefdb0c8fe5e3
hoyun06 <hoyun06@gmail.com>
2024-01-28 19:34:28 +0900

    feat: SubCommentModifyRequest dto 생성 (#56)

---
+++ src/main/java/com/codot/link/domains/comment/dto/request/SubCommentModifyRequest.java
@@ -0,0 +1,27 @@
+package com.codot.link.domains.comment.dto.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SubCommentModifyRequest {
+
+	@NotEmpty(message = "수정할 대댓글 내용은 필수이며 공백 문자열을 허용되지 않습니다.")
+	private String content;
+
+	// === Test 용도 === //
+	@Builder(access = AccessLevel.PRIVATE)
+	private SubCommentModifyRequest(String content) {
+		this.content = content;
+	}
+
+	public static SubCommentModifyRequest from(String content) {
+		return SubCommentModifyRequest.builder()
+			.content(content)
+			.build();
+	}
+}
