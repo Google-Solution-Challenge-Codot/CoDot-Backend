6b407bcd08e7c22a1f26762f99919efac6cf4b81
hoyun06 <hoyun06@gmail.com>
2024-01-28 16:49:18 +0900

    feat: CommentModifyRequest dto 생성 (#55)

---
+++ src/main/java/com/codot/link/domains/comment/dto/request/CommentModifyRequest.java
@@ -0,0 +1,27 @@
+package com.codot.link.domains.comment.dto.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentModifyRequest {
+
+	@NotEmpty(message = "수정할 댓글 내용은 필수이며 공백 문자열을 허용되지 않습니다.")
+	private String content;
+
+	// === Test 용도 === //
+	@Builder(access = AccessLevel.PRIVATE)
+	private CommentModifyRequest(String content) {
+		this.content = content;
+	}
+
+	public static CommentModifyRequest from(String content) {
+		return CommentModifyRequest.builder()
+			.content(content)
+			.build();
+	}
+}
