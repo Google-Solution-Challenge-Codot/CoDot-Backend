69e6b6b6e27ec56e39c9880ce705cc9f884a29ef
hoyun06 <hoyun06@gmail.com>
2024-01-28 16:49:52 +0900

    feat: CommentRepository COMMENT2_MODIFY1 관련 조회 메서드 추가 (#55)

--- src/main/java/com/codot/link/domains/comment/repository/CommentRepository.java
+++ src/main/java/com/codot/link/domains/comment/repository/CommentRepository.java
@@ -1,8 +1,12 @@
 package com.codot.link.domains.comment.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codot.link.domains.comment.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+	Optional<Comment> findByIdAndUser_Id(Long commentId, Long userId);
 }
