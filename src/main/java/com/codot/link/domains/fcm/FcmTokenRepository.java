5e1fad904a53ef4972185a184279ca95a0b3c47c
hoyun06 <hoyun06@gmail.com>
2024-02-04 17:40:51 +0900

    feat: FcmTokenRepository 메서드 추가 (#79)

---
+++ src/main/java/com/codot/link/domains/fcm/FcmTokenRepository.java
@@ -0,0 +1,21 @@
+package com.codot.link.domains.fcm;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.codot.link.domains.fcm.domain.FcmToken;
+import com.codot.link.domains.user.domain.User;
+
+public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
+
+	Optional<FcmToken> findByUser(User user);
+
+	@Query(value = "select * from fcm_token ft "
+		+ "join group_user gu on ft.user_id = gu.user_id "
+		+ "where gu.group_id = :groupId and gu.accepted = true", nativeQuery = true)
+	List<FcmToken> findAllByGroupId(@Param("groupId") Long groupId);
+}
