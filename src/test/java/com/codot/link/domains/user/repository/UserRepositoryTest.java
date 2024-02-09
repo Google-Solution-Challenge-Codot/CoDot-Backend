986634cd7bddc005ad45353a9b9bd79747a3fa63
hoyun06 <hoyun06@gmail.com>
2024-02-09 19:43:17 +0900

    test: UserRepository findUserByDirectSearchTwoHop 테스트 작성 (#91)

--- src/test/java/com/codot/link/domains/user/repository/UserRepositoryTest.java
+++ src/test/java/com/codot/link/domains/user/repository/UserRepositoryTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import com.codot.link.domains.link.domain.Link;
 import com.codot.link.domains.link.repository.LinkRepository;
 import com.codot.link.domains.user.dao.OneHopDirectSearchResult;
+import com.codot.link.domains.user.dao.TwoHopDirectSearchResult;
 import com.codot.link.domains.user.domain.User;
 import com.codot.link.domains.user.dto.request.UserSignupRequest;
 
@@ -115,6 +116,99 @@ class UserRepositoryTest {
 
 		//when
 		Optional<OneHopDirectSearchResult> targetUser = userRepository.findUserByDirectSearchFirstHop(userA.getId(),
+			"nicknameC");
+
+		//then
+		assertThrows(NoSuchElementException.class, targetUser::get);
+	}
+
+	@Test
+	@DisplayName("Direct Search 로직에서 second hop에 있는 사용자를 닉네임으로 조회할 수 있어야 한다")
+	void Direct_Search_로직에서_second_hop에_있는_사용자를_닉네임으로_조회할_수_있어야_한다() {
+		//given
+		UserSignupRequest requestA = UserSignupRequest.of("userA", "email", "nicknameA", "HONGIK", "CE",
+			"hello", UNDERGRADUATE);
+		UserSignupRequest requestB = UserSignupRequest.of("userB", "email", "nicknameB", "HONGIK", "CE",
+			"hello", UNDERGRADUATE);
+		UserSignupRequest requestC = UserSignupRequest.of("userC", "email", "nicknameC", "HONGIK", "CE",
+			"hello", UNDERGRADUATE);
+
+		User userA = userRepository.save(User.from(requestA));
+		User userB = userRepository.save(User.from(requestB));
+		User userC = userRepository.save(User.from(requestC));
+
+		Link fromAToB = Link.of(userA, userB, CONNECTED);
+		Link fromBToA = Link.of(userB, userA, CONNECTED);
+		Link fromBToC = Link.of(userB, userC, CONNECTED);
+		Link fromCToB = Link.of(userC, userB, CONNECTED);
+		linkRepository.saveAll(
+			List.of(fromAToB, fromBToA, fromBToC, fromCToB));
+
+		//when
+		TwoHopDirectSearchResult targetUser = userRepository.findUserByDirectSearchTwoHop(userA.getId(),
+			"nicknameC").orElseThrow(NoSuchElementException::new);
+
+		//then
+		assertThat(targetUser.getFirstHopId()).isEqualTo(userB.getId());
+		assertThat(targetUser.getSecondHopId()).isEqualTo(userC.getId());
+	}
+
+	@Test
+	@DisplayName("해당 닉네임을 가진 사용자가 존재하더라도 2 hop 에 없다면 조회하지 않아야 한다")
+	void 해당_닉네임을_가진_사용자가_존재하더라도_2_hop_에_없다면_조회하지_않아야_한다() {
+		//given
+		UserSignupRequest requestA = UserSignupRequest.of("userA", "email", "nicknameA", "HONGIK", "CE",
+			"hello", UNDERGRADUATE);
+		UserSignupRequest requestB = UserSignupRequest.of("userB", "email", "nicknameB", "HONGIK", "CE",
+			"hello", UNDERGRADUATE);
+		UserSignupRequest requestC = UserSignupRequest.of("userC", "email", "nicknameC", "HONGIK", "CE",
+			"hello", UNDERGRADUATE);
+
+		User userA = userRepository.save(User.from(requestA));
+		User userB = userRepository.save(User.from(requestB));
+		User userC = userRepository.save(User.from(requestC));
+
+		Link fromAToB = Link.of(userA, userB, CONNECTED);
+		Link fromBToA = Link.of(userB, userA, CONNECTED);
+		Link fromBToC = Link.of(userB, userC, CONNECTED);
+		Link fromCToB = Link.of(userC, userB, CONNECTED);
+		linkRepository.saveAll(
+			List.of(fromAToB, fromBToA, fromBToC, fromCToB));
+
+		//when
+		Optional<TwoHopDirectSearchResult> targetUser = userRepository.findUserByDirectSearchTwoHop(userA.getId(),
+			"nicknameB");
+
+		//then
+		assertThrows(NoSuchElementException.class, targetUser::get);
+	}
+
+	@Test
+	@DisplayName("해당 닉네임을 가진 사용자와 1 hop 연결도 있고 2 hop 연결도 있다면 해당 사용자를 조회하지 않아야 한다")
+	void 해당_닉네임을_가진_사용자와_1_hop_연결도_있고_2_hop_연결도_있다면_해당_사용자를_조회하지_않아야_한다() {
+		//given
+		UserSignupRequest requestA = UserSignupRequest.of("userA", "email", "nicknameA", "HONGIK", "CE",
+			"hello", UNDERGRADUATE);
+		UserSignupRequest requestB = UserSignupRequest.of("userB", "email", "nicknameB", "HONGIK", "CE",
+			"hello", UNDERGRADUATE);
+		UserSignupRequest requestC = UserSignupRequest.of("userC", "email", "nicknameC", "HONGIK", "CE",
+			"hello", UNDERGRADUATE);
+
+		User userA = userRepository.save(User.from(requestA));
+		User userB = userRepository.save(User.from(requestB));
+		User userC = userRepository.save(User.from(requestC));
+
+		Link fromAToB = Link.of(userA, userB, CONNECTED);
+		Link fromBToA = Link.of(userB, userA, CONNECTED);
+		Link fromBToC = Link.of(userB, userC, CONNECTED);
+		Link fromCToB = Link.of(userC, userB, CONNECTED);
+		Link fromAToC = Link.of(userA, userC, CONNECTED);
+		Link fromCToA = Link.of(userC, userA, CONNECTED);
+		linkRepository.saveAll(
+			List.of(fromAToB, fromBToA, fromBToC, fromCToB, fromAToC, fromCToA));
+
+		//when
+		Optional<TwoHopDirectSearchResult> targetUser = userRepository.findUserByDirectSearchTwoHop(userA.getId(),
 			"nicknameC");
 
 		//then
