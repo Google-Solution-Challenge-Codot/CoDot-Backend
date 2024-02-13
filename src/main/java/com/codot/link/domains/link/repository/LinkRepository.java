ac33fd68c24b61df195f2df3dbc7fcf898492985
hoyun06 <hoyun06@gmail.com>
2024-02-13 17:01:33 +0900

    refactor: LinkRepository findTwoHops 쿼리 수정 (#95)

--- src/main/java/com/codot/link/domains/link/repository/LinkRepository.java
+++ src/main/java/com/codot/link/domains/link/repository/LinkRepository.java
@@ -24,12 +24,14 @@ public interface LinkRepository extends JpaRepository<Link, Long> {
 	Optional<Link> findByFrom_IdAndTo_IdAndStatus(Long fromId, Long toId, Status status);
 
 	@Query(value =
-		"select l1.* from (select u.* from users u join link l on u.user_id = l.from_id where l.to_id = :userId and l.status = 'CONNECTED') as u1 "
+		"select l1.* from ("
+			+ "select u.* from users u where exists (select 1 from link l where l.from_id = :userId and l.to_id = u.user_id and l.status = 'CONNECTED')"
+			+ ") as u1 "
 			+ "join link l1 on l1.from_id = u1.user_id "
-			+ "where l1.to_id <> :userId and l1.status = 'CONNECTED' "
+			+ "where l1.to_id <> :userId "
 			+ "and not exists "
-			+ "(select 1 from link l2 where (l2.from_id = :userId and l2.to_id = l1.to_id and l2.status = 'CONNECTED') or "
-			+ "(l2.from_id = l1.to_id and l2.to_id = :userId and l2.status = 'CONNECTED')) "
+			+ "(select 1 from link l2 where l2.from_id = :userId and l2.to_id = l1.to_id and l2.status = 'CONNECTED') "
+			+ "and l1.status = 'CONNECTED' "
 			+ "order by random() "
 			+ "limit 5", nativeQuery = true)
 	List<Link> findTwoHops(@Param("userId") Long userId);
